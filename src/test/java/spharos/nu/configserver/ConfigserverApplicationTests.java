78d0c33784d20cbed7fdb5f729999914cce24ecd
SEOIHYEON <tjwn1408@naver.com>
2024-05-23 13:16:50 +0900

    Update: @SpringBootTest 어노테이션 주ì석처리

--- src/test/java/spharos/nu/configserver/ConfigserverApplicationTests.java
+++ src/test/java/spharos/nu/configserver/ConfigserverApplicationTests.java
@@ -3,7 +3,7 @@ package spharos.nu.configserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ConfigserverApplicationTests {
 
 	@Test
